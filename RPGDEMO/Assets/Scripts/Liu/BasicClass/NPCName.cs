3d7ee9688f11849cbf270305f8cc262b53b26fac
AshesFeathers <1206996078@qq.com>
2019-12-07 13:18:39 +0800

    名字平滑移动

--- RPGDEMO/Assets/Scripts/Liu/BasicClass/NPCName.cs
+++ RPGDEMO/Assets/Scripts/Liu/BasicClass/NPCName.cs
@@ -3,32 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// 将物体如NPC的名字显示在UI界面上
+/// </summary>
 public class NPCName : MonoBehaviour {
-    public string Name;
-    public GameObject Text;
-    private GameObject text;
-    private float npcHeight;
-    private Collider collider;
+    public string Name;         //要显示的文本
+    public GameObject Text;     //游戏中的文本对象预制体
+    private GameObject text;    //游戏中的文本对象实体
+    private Collider collider;  //物体模型的Collider
+    private float npcHeight;    //物体模型的高度
 
-    public bool isRendering;
-    float curtTime = 0f;
-    float lastTime = 0f;
+    public bool isRendering;    //是否渲染，是否在摄像机前
+    float curtTime = 0f;        //最近时间
+    float lastTime = 0f;        //最后时间
     void Start() {
-        float size_y;
-        float scal_y;
-        collider = GetComponent<Collider>();
+        float size_y;           //模型y轴高度
+        float scal_y;           //模型y轴缩放比例
+        collider = GetComponent<Collider>();    //物品模型Collider组件
 
         size_y = collider.bounds.size.y;
         scal_y = collider.transform.localScale.y;
         npcHeight = (size_y * scal_y);
-        text = GameObject.Instantiate(Text, GameObject.Find("NPCName").transform);
-        text.name = Name;
-        text.transform.GetChild(0).GetComponent<Text>().text = Name;
 
+        text = GameObject.Instantiate(Text, GameObject.Find("NPCName").transform);      //通过文本对象预制体复制一个实体，父物体设置为“NPCName”
+        text.name = Name;   //文本对象名
+        text.transform.GetChild(0).GetComponent<Text>().text = Name;    //文本对象内容
+        //物体头顶位置3d坐标
         Vector3 worldPosition = new Vector3(collider.transform.position.x, collider.transform.position.y + npcHeight, collider.transform.position.z);
-        Vector2 position = Camera.main.WorldToScreenPoint(worldPosition);
+        Vector2 position = Camera.main.WorldToScreenPoint(worldPosition);   //转换为2d屏幕坐标
 
-        text.transform.position = position;
+        text.transform.position = position;     //移动到指定坐标
         text.SetActive(false);
     }
 
@@ -39,26 +43,26 @@ public class NPCName : MonoBehaviour {
             Vector3 worldPosition = new Vector3(collider.transform.position.x, collider.transform.position.y + npcHeight, collider.transform.position.z);
             //根据NPC头顶的3D坐标换算成它在2D屏幕中的坐标
             Vector2 targetPosition = Camera.main.WorldToScreenPoint(worldPosition);
-
+            //物体是否在摄像机前
+            Vector2 position = text.transform.position;
             if (IsInView(worldPosition)) {
                 if (text != null) {
-                    //Debug.Log(Vector3.Distance(GameObject.Find("Hoshi").transform.position, collider.transform.position));
+                    //主角与物体之间距离小于20
                     if (Vector3.Distance(GameObject.Find("Hoshi").transform.position, collider.transform.position) <= 20) {
                         if (!text.activeSelf) {
-                            //Debug.Log("近True");
                             text.SetActive(true);
                         }
                     } else {
                         if (text.activeSelf) {
-                            //Debug.Log("远False");
                             text.SetActive(false);
                         }
                     }
                 }
-            text.transform.position = targetPosition;
+                position.x = Mathf.Lerp(position.x, targetPosition.x, 0.5f);
+                position.y = Mathf.Lerp(position.y, targetPosition.y, 0.5f);
+                text.transform.position = position;
             } else {
                 if (text.activeSelf) {
-                    //Debug.Log("不在摄像机前");
                     text.SetActive(false);
                 }
             }

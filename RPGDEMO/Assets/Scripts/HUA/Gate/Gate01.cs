2604e80f4eaec384ea3855ceaee9459b3c0be4f8
zxc471313304 <52726197+zxc471313304@users.noreply.github.com>
2019-12-07 18:28:37 +0800

    异步加载细节打磨

--- RPGDEMO/Assets/Scripts/HUA/Gate/Gate01.cs
+++ RPGDEMO/Assets/Scripts/HUA/Gate/Gate01.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Gate01 : MonoBehaviour
 {
     private GameObject Player;
+    private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
         Player = GameObject.FindWithTag("TestPlayer");
     }
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "TestPlayer")
         {
+            mainCamera.cullingMask=1<<1;
+            mainCamera.backgroundColor = Color.black;
+            mainCamera.clearFlags = CameraClearFlags.SolidColor;
+            DontDestroyOnLoad(Player);
+            SceneManager.LoadScene("CheckPoint 1");
             transformChange();
         }
     }
     void transformChange()
     {
-        Player.transform.position = new Vector3(22, 0, -215);
+        Player.transform.position = new Vector3(21, 0, -215.5f);
     }
     // Update is called once per frame
     void Update()
--- RPGDEMO/Assets/Scripts/HUA/Gate/Gate02.cs
+++ RPGDEMO/Assets/Scripts/HUA/Gate/Gate02.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class Gate02 : MonoBehaviour
 {
     private GameObject Player;
+    private Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
+        mainCamera = Camera.main;
         Player = GameObject.FindWithTag("TestPlayer");
     }
     void OnTriggerEnter(Collider collider)
     {
         if (collider.tag == "TestPlayer")
         {
+            mainCamera.cullingMask = 1 << 1;
+            mainCamera.backgroundColor = Color.black;
+            mainCamera.clearFlags = CameraClearFlags.SolidColor;
+            DontDestroyOnLoad(Player);
+            SceneManager.LoadSceneAsync("LoadingScene");
             transformChange();
         }
     }

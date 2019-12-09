2fd4580b9ff64880fbc770e99e7b10fea22edd8b
AshesFeathers <1206996078@qq.com>
2019-12-09 17:52:59 +0800

    物品的拖拽与简单堆叠

--- RPGDEMO/Assets/Scripts/Liu/BasicClass/DataManager.cs
+++ RPGDEMO/Assets/Scripts/Liu/BasicClass/DataManager.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 数据类，存储物品，网格信息等，方便索引查找，做持久化操作等
+/// </summary>
 public class DataManager : MonoBehaviour
 {
-    public static GameObject[] SlotGOList = new GameObject[80];
-    public static GameObject[] ItemGOList = new GameObject[80];
+    public static GameObject[] SlotGOList = new GameObject[80];     //存放所有的Slot游戏对象
+    public static GameObject[] ItemGOList = new GameObject[80];     //存放所有的Item游戏对象
 }

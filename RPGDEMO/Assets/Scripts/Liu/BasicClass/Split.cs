03998951e1cba5dcdb0782dcd4110f52cf4d66a1
AshesFeathers <1206996078@qq.com>
2019-12-10 13:09:15 +0800

    按输入数量拆分

--- RPGDEMO/Assets/Scripts/Liu/BasicClass/Split.cs
+++ RPGDEMO/Assets/Scripts/Liu/BasicClass/Split.cs
@@ -7,34 +7,45 @@ using UnityEngine.UI;
 public class Split : MonoBehaviour {
     public static Slot slot;
     public static Item item;
-    public int Count = 0;
+    public int Count = 1;
 
-    private Text tipText;
+    public string tiptext;
+    private Text TipText;
     void Start() {
-        tipText = this.transform.GetComponentInChildren<Text>();
-        Debug.Log(tipText);
-
-        tipText.text = "物品" + Count;
+        TipText = this.transform.GetComponentInChildren<Text>();
     }
-
     void Update() {
-
+        tiptext = "物品UID：" + item.itemBase.UID + "数量 * " + Count.ToString();
+        TipText.text = tiptext;
+    }
+    public void DeSplit() {
+        slot.SplitItem(item, Count);
+        GameObject.Find("Split").SetActive(false);
     }
 
-    public void DeSplit() {
-        SetCount(Convert.ToInt32(this.transform.GetComponent<Text>().text));
-        slot.SplitItem(item, Count);
+    public void Cancel() {
+        GameObject.Find("Split").SetActive(false);
     }
 
     public void AddCount() {
-        SetCount(Count++);
+        Debug.Log("Add");
+        SetCount(++Count);
     }
 
     public void DecCount() {
-        SetCount(Count--);
+        Debug.Log("Dec");
+        SetCount(--Count);
+    }
+
+    public void ChangeCount() {
+        Debug.Log("ChangeCount");
+        if (this.transform.GetComponent<InputField>().text != "") {
+            SetCount(Convert.ToInt32(this.transform.GetComponent<InputField>().text));
+        }
     }
 
     private void SetCount(int count) {
         Count = Mathf.Clamp(count, 1, item.curStack - 1);
+        this.transform.GetComponent<InputField>().text = Count.ToString();
     }
 }

375215cfef00d432994d8e7b864d859c523149ba
zxc471313304 <52726197+zxc471313304@users.noreply.github.com>
2019-12-23 13:37:15 +0800

    游戏退出功能制作完毕

---
+++ RPGDEMO/Assets/Scripts/HUA/Exit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void exit()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
